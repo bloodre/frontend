9f66f221504b19166aa52e9fc2d9114b31cd3e19
bloodre <mathieu87100@gmail.com>
2021-09-13 16:21:19 +0900

    rotate patch
    removed the page reload when the screen is rotated.

--- public/js/home.js
+++ public/js/home.js
@@ -1,8 +1,13 @@
 var slideIndex = 1;
+var rotationCount = 0;
+var heightState = [];
+var carouselHeightState = [];
+var typesearch_baseheight = null;
 
 $(function(){
 
   showSlides(slideIndex);
+  typesearch_baseheight = $('#typesearch').height()+'px';
   let width = $(window).width();
 
 
@@ -12,8 +17,14 @@ $(function(){
   fitHeight(width);
 
   //Remove previous carousel and create new one
-  $(window).bind('orientationchange', function (event) {
-    location.reload(true);
+  $(window).on('orientationchange', function (event) {
+    rotationCount += 1;
+    width = $(window).width();
+
+    clearCarousel();
+    displayCarousel(width);
+    fitHeight(width);
+    fixCarouselWidth();
   });
 
   // pin-button code \\
@@ -108,45 +119,76 @@ function displayCarousel(width){
   });
 }
 
+function clearCarousel(){
+  $('.item').each(function(){
+    let children = $(this).children();
+    for(let k=1;k<=3;k++){
+      children.eq(k).remove();
+    }
+  });
+}
+
 
 
 // Align the search boxes vertically
 function fitHeight(width){
-  if(992<width){
-    let new_margin = $('#typesearch').height() - $('#makersearch').height() - $('#wordsearch').height();
-    if(0<new_margin){
-      $('#wordsearch').css('margin-top',new_margin+'px');
+  if(rotationCount <= 1){
+    if(992<width){
+      let new_margin = $('#typesearch').height() - $('#makersearch').height() - $('#wordsearch').height();
+      if(0<new_margin){
+        $('#wordsearch').css('margin-top',new_margin-2+'px');
+      }
+      else{
+        let margin_top = parseFloat($('#wordsearch').css('margin-top').split('p')[0]);
+        let new_height = margin_top+$('#makersearch').height() + $('#wordsearch').height();
+        $('#typesearch').height(new_height+2+'px');    
+      }
     }
-    else{
-      let margin_top = parseFloat($('#wordsearch').css('margin-top').split('p')[0]);
-      new_margin = margin_top+$('#makersearch').height() + $('#wordsearch').height();
-      $('#typesearch').height(new_margin+'px');    
+    else if(rotationCount==1){
+      $('#typesearch').height('auto');
     }
+    heightState[rotationCount] = [$('#typesearch').height()+'px',parseFloat($('#wordsearch').css('margin-top'))];
+  }
+  else{
+    $('#typesearch').height(heightState[rotationCount%2][0]);
+    $('wordsearch').css('margin-top',heightState[rotationCount%2][1]);
   }
 }
 
 function fixCarouselWidth(){
-  let block_width = $('.block').eq(0).width();
-  let max_overflow_img = null;
-  let overflow_block_count = 0;
-
-  $('.block').each(function(){
-    if(block_width < $(this).find('img').eq(0).width()){
-      overflow_block_count += 1;
-      if(overflow_block_count==1){
-        max_overflow_img = $(this).find('img').eq(0);
-      }
-      else if(max_overflow_img.width() < $(this).find('img').eq(0).width()){
-        max_overflow_img = $(this).find('img').eq(0);
-      }
-    }
-  });
-  if(max_overflow_img != null){
-    max_overflow_img.width(block_width+'px');
-    let new_height = max_overflow_img.height();
+  if(rotationCount <= 1){
+    let block_width = $('.block').eq(0).width();
+    let max_overflow_img = null;
+    let overflow_block_count = 0;
 
     $('.block').each(function(){
-      $(this).find('img').eq(0).css('max-height',new_height+'px');
+      if(block_width < $(this).find('img').eq(0).width()){
+          overflow_block_count += 1;
+          if(overflow_block_count==1){
+            max_overflow_img = $(this).find('img').eq(0);
+          }
+          else if(max_overflow_img.width() < $(this).find('img').eq(0).width()){
+            max_overflow_img = $(this).find('img').eq(0);
+          }
+        }
+      });
+      if(max_overflow_img != null){
+        max_overflow_img.width(block_width+'px');
+        let new_height = max_overflow_img.height();
+        carouselHeightState[rotationCount] = new_height+'px';
+
+        $('.block').each(function(){
+          $(this).find('img').eq(0).css('max-height',new_height+'px');
+          $(this).find('img').eq(0).css('width','auto');
+        });
+      }
+      else{
+        carouselHeightState[rotationCount] = $('.block').find('img').eq(0).height()+'px';
+      }
+  }
+  else{
+    $('.block').each(function(){
+      $(this).find('img').eq(0).css('max-height',carouselHeightState[rotationCount%2]);
       $(this).find('img').eq(0).css('width','auto');
     });
   }
